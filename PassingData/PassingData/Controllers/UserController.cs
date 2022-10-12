723e8bce2bc316bb756850d44413cba170aaf79b
abdullahayhan <aayhaan4@gmail.com>
2022-10-12 17:30:30 +0300

    viewDataExamp

--- PassingData/PassingData/Controllers/UserController.cs
+++ PassingData/PassingData/Controllers/UserController.cs
@@ -9,19 +9,28 @@ namespace PassingData.Controllers
 {
     public class UserController : Controller
     {
-        public IActionResult UseViewBag()
-        {
-            ViewBag.UserName = "Abdullah";
-            User user = new User() { FirstName = "Çidem", LastName = "Ayhan" };
-            ViewBag.User = user;
-            List<User> users = new List<User>()
+        List<User> users = new List<User>()
             {
                 new User(){FirstName="Mert",LastName="Ayhan"},
                 new User(){FirstName="Elif",LastName="Ayhan"},
                 new User(){FirstName="Aşkın",LastName="Ayhan"},
 
             };
+        public IActionResult UseViewBag()
+        {
+            ViewBag.UserName = "Abdullah";
+            User user = new User() { FirstName = "Çidem", LastName = "Ayhan" };
+            ViewBag.User = user;
             ViewBag.Users = users;
+            return View();
+        }
+
+        public IActionResult UseViewData()
+        {
+            ViewData["UserName"] = "Abdullah";
+            User user = new User() { LastName = "Ayhan", FirstName = "Mert" };
+            ViewData["User"] = user;
+            ViewData["Users"] = users;
             return View();
         }
     }
