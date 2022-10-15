53b2c361c2eaa3a08bae41d81c4305eedf2579f2
abdullahayhan <aayhaan4@gmail.com>
2022-10-15 16:23:24 +0300

    Partial View Example

---
+++ ViewStructure/ViewStructure/Controllers/TeacherController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ViewStructure.Models;
+
+namespace ViewStructure.Controllers
+{
+    public class TeacherController : Controller
+    {
+        List<Teacher> teachers = new List<Teacher>() 
+        {
+            new Teacher(){ID=1,Name="Abdullah",Gender="man"},
+            new Teacher(){ID=3,Name="Mert",Gender="man"},
+            new Teacher(){ID=4,Name="Selin",Gender="woman"},
+            new Teacher(){ID=5,Name="Merve",Gender="woman"},
+        };
+        public IActionResult GetTeacherList()
+        {
+            return View(teachers);
+        }
+
+        public IActionResult DeleteTeacher(int id)
+        {
+            Teacher teacher = teachers.Find(x => x.ID == id);
+            return View(teacher);
+        }
+    }
+}
--- ViewStructure/ViewStructure/Startup.cs
+++ ViewStructure/ViewStructure/Startup.cs
@@ -25,7 +25,7 @@ namespace ViewStructure
             {
                 app.UseDeveloperExceptionPage();
             }
-
+            app.UseStaticFiles();
             app.UseRouting();
 
             app.UseEndpoints(endpoints =>
